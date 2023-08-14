ddd6fb2c7f3d3cfbb1089d85eb5b89c46b2f015b
Max Schafer <maxsg5@hotmail.com>
2023-08-13 19:46:01 -0600

    init commit

---
+++ client/client.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"log"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	pb "grpc-simplechat/server/api" // Adjust this import path based on your actual project structure
+)
+
+func main() {
+	// Load the server's CA certificate
+	creds, err := credentials.NewClientTLSFromFile("../scripts/localhost.crt", "")
+	if err != nil {
+		log.Fatalf("failed to load server's CA certificate: %v", err)
+	}
+
+	// Set up a connection to the server with the credentials
+	conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(creds))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewMyServiceClient(conn)
+
+	// Example call to the server
+	response, err := c.GetMessage(context.Background(), &pb.MessageRequest{})
+	if err != nil {
+		log.Fatalf("could not get message: %v", err)
+	}
+	log.Printf("Response from server: %s", response.Message)
+}
---
+++ server/server.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"crypto/tls"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	pb "grpc-simplechat/server/api"
+)
+
+type server struct {
+	pb.UnimplementedMyServiceServer
+}
+
+func (s *server) GetMessage(ctx context.Context, in *pb.MessageRequest) (*pb.MessageResponse, error) {
+	return &pb.MessageResponse{Message: "Hello from gRPC server!"}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", "localhost:50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	// Load server's certificate and private key from the scripts directory
+	cert, err := tls.LoadX509KeyPair("../scripts/localhost.crt", "../scripts/localhost.key")
+	if err != nil {
+		log.Fatalf("failed to load server's certificate and key: %v", err)
+	}
+
+	// Create the TLS credentials for transport
+	creds := credentials.NewTLS(&tls.Config{
+		Certificates: []tls.Certificate{cert},
+	})
+
+	s := grpc.NewServer(grpc.Creds(creds))
+	pb.RegisterMyServiceServer(s, &server{})
+    log.Println("Server started and listening on localhost:50051")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
